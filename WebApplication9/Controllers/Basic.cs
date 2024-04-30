8aad05d98c97ab4d34a59d3c77da75b03c608ba9
Gauri <gaurijindal.hrd@gmail.com>
2024-04-30 11:39:42 +0530

    Add project files.

---
+++ WebApplication9/Controllers/Basic.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication9.Controllers
+{
+    public class Basic : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [Route("sample/message")]
+        public IActionResult Message()
+        {
+            return View();
+        }
+    }
+}
